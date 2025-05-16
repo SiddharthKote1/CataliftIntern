91af1618fa6f66b5d8aeff250f10c61eb58bcc14
Siddharth Kote <Siddharthkote128@gmail.com>
2025-05-16 18:43:59 +0530

    Gallary work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.compose.material:material-icons-extended:1.6.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/cataliftintern/CataliftPage.kt
+++ app/src/main/java/com/example/cataliftintern/CataliftPage.kt
@@ -1,81 +1,84 @@
 package com.example.cataliftintern
 
+import android.Manifest
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Chat
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.AddBox
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Chat
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.AddBox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import androidx.core.net.toUri
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CataPage() {
     var search by remember { mutableStateOf("") }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val context = LocalContext.current
+    var showImagePickerDialog by remember { mutableStateOf(false)}
+
+    val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+        bitmap?.let {
+            val uri = MediaStore.Images.Media.insertImage(
+                context.contentResolver,
+                it,
+                "profile",
+                null
+            ).toUri()
+            imageUri = uri
+        }
+    }
+
+    val galleryLauncher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
+            imageUri = uri
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {
-                    Row(
-                        verticalAlignment = Alignment.Bottom // Align children by their bottom edge
-                    ) {
+                    Row(verticalAlignment = Alignment.Bottom) {
                         Text(
                             "CATA ",
                             style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
                             color = Color.White
                         )
-                        Spacer(modifier = Modifier.width(0.1.dp))
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.padding(bottom = 0.5.dp) // Adjust to align text baseline if needed
-                        ) {
+                        Spacer(modifier = Modifier.width(2.dp))
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
                             Box(
                                 modifier = Modifier
                                     .width(30.dp)
                                     .height(2.dp)
                                     .background(Color.White)
                             )
-                            Spacer(modifier = Modifier.height(2.dp)) // small gap between line and text
+                            Spacer(modifier = Modifier.height(2.dp))
                             Text(
                                 "LIFT",
                                 style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
@@ -83,31 +86,30 @@ fun CataPage() {
                             )
                         }
                     }
-
                 },
                 actions = {
-                    IconButton(onClick = { /* Account */ }) {
+                    IconButton(onClick = { }) {
                         Icon(
                             imageVector = Icons.Default.AccountCircle,
                             contentDescription = "Account",
-                            modifier = Modifier.size(28.dp),
-                            tint = Color.White
+                            tint = Color.White,
+                            modifier = Modifier.size(28.dp)
                         )
                     }
-                    IconButton(onClick = { /* Account */ }) {
+                    IconButton(onClick = { }) {
                         Icon(
                             imageVector = Icons.Default.Notifications,
-                            contentDescription = "Account",
-                            modifier = Modifier.size(28.dp),
-                            tint = Color.White
+                            contentDescription = "Notifications",
+                            tint = Color.White,
+                            modifier = Modifier.size(28.dp)
                         )
                     }
-                    IconButton(onClick = { /* Chat */ }) {
+                    IconButton(onClick = { }) {
                         Icon(
-                            imageVector = Icons.Default.Chat,
+                            imageVector = Icons.AutoMirrored.Filled.Chat,
                             contentDescription = "Chat",
-                            modifier = Modifier.size(28.dp),
-                            tint = Color.White
+                            tint = Color.White,
+                            modifier = Modifier.size(28.dp)
                         )
                     }
                 },
@@ -117,60 +119,125 @@ fun CataPage() {
             )
         }
     ) { innerPadding ->
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(innerPadding),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp, vertical = 20.dp)
         ) {
-            Spacer(modifier = Modifier.height(50.dp))
-            Row() {
+            Row {
                 OutlinedTextField(
                     value = search,
                     onValueChange = { search = it },
-                    label = {
-                        Text("Search")
-                    },
+                    label = { Text("Search") },
                     leadingIcon = {
                         Icon(
                             imageVector = Icons.Default.Search,
-                            contentDescription = null
+                            contentDescription = "Search"
                         )
                     },
-                    modifier = Modifier
-                        .width(300.dp)
-                        .height(80.dp)
-                        .padding(16.dp),
+                    modifier = Modifier.height(64.dp),
                     singleLine = true,
                     shape = RoundedCornerShape(30.dp)
                 )
-            }
-            Spacer(modifier = Modifier.width(40.dp))
-            IconButton(
-                onClick = {},
-            ) {
+
+                Spacer(modifier = Modifier.width(30.dp))
+
                 Box(
                     modifier = Modifier
-                        .size(50.dp)
-                        .background(Color.White)
+                        .size(60.dp)
+                        .background(Color.White, shape = RoundedCornerShape(12.dp)),
+                    contentAlignment = Alignment.Center
                 ) {
-                    Icon(
-                        imageVector = Icons.Outlined.AddBox,
-                        contentDescription = "Add",
-                        tint = Color.Black,
-                        modifier = Modifier
-                            .size(40.dp)
-                            .align(Alignment.Center)
-                    )
+                    IconButton(onClick = {
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.AddBox,
+                            contentDescription = "Add",
+                            tint = Color.Black,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
                 }
             }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clickable {
+                                showImagePickerDialog=true
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (imageUri != null) {
+                            AsyncImage(
+                                model = imageUri,
+                                contentDescription = "Profile Picture",
+                                modifier = Modifier
+                                    .size(48.dp)
+                                    .clip(CircleShape)
+                                    .background(Color.Gray, shape = CircleShape),
+                                contentScale = ContentScale.Crop
+                            )
+                        } else {
+                            Icon(
+                                imageVector = Icons.Default.AccountCircle,
+                                contentDescription = null,
+                                tint = Color.Gray,
+                                modifier = Modifier.size(48.dp)
+                                    .clip(CircleShape)
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Column {
+                        Text("Akhilesh Yadav", fontWeight = FontWeight.Bold)
+                        Text(
+                            "Founder at Google • 1d • Edited",
+                            fontSize = 12.sp,
+                            color = Color.Gray
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Icon(Icons.Default.Home, contentDescription = null)
+                }
+            }
+        }
+        if (showImagePickerDialog) {
+            AlertDialog(
+                onDismissRequest = { showImagePickerDialog = false },
+                title = { Text("Select Image Source") },
+                text = { Text("Choose an option to set your profile picture") },
+                confirmButton = {
+                    TextButton(onClick = {
+                        showImagePickerDialog = false
+                    }) {
+                        Text("Take Photo")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        showImagePickerDialog = false
+                        galleryLauncher.launch("image/*")
+                    }) {
+                        Text("Choose from Gallery")
+                    }
+                }
+            )
         }
     }
 }
 
-@Preview(showBackground = true) 
+@Preview(showBackground = true)
 @Composable
-fun CataPagePreview(){
+fun CataPagePreview() {
     CataPage()
 }
